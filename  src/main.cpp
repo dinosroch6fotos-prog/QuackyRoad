faed077ffe1c80b8ceddfe29e6c610f9547c9e6b
dinosroch6fotos-prog <dinos.roch6fotos@gmail.com>
2026-07-30 11:32:22 -0600

    QUACK!

---  src/main.cpp
+++  src/main.cpp
@@ -3,12 +3,18 @@
 
 int main() {
 
-    InitWindow(800, 450, "QuackyRoad");
+         InitWindow(800, 450, "QuackyRoad");
+         SetTargetFPS(60);
+    
+         //Loop Principal que mantiene al juego activo hasta que se cierre la ventana
+         while(!WindowShouldClose()){
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            EndDrawing();
+         }
+         
+         CloseWindow(); // Cierra la ventana y libera recursos
 
-
-
-
-
-
+        
     return 0;
 }
