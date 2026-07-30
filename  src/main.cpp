65639bc7c8ba627f6b8eee9afd06a894a1a145b4
dinosroch6fotos-prog <dinos.roch6fotos@gmail.com>
2026-07-30 11:41:08 -0600

    QUACK!

---  src/main.cpp
+++  src/main.cpp
@@ -3,18 +3,31 @@
 
 int main() {
 
-         InitWindow(800, 450, "QuackyRoad");
-         SetTargetFPS(60);
-    
-         //Loop Principal que mantiene al juego activo hasta que se cierre la ventana
-         while(!WindowShouldClose()){
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
-            EndDrawing();
-         }
-         
-         CloseWindow(); // Cierra la ventana y libera recursos
+    InitWindow(800, 450, "QuackyRoad");
+    SetTargetFPS(60);
 
-        
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // dónde está la cámara
+    camera.target   = (Vector3){ 0.0f, 0.0f, 0.0f };    // hacia dónde mira
+    camera.up       = (Vector3){ 0.0f, 1.0f, 0.0f };    // qué dirección es "arriba"
+    camera.fovy     = 45.0f;                             // ángulo de visión (zoom)
+    camera.projection = CAMERA_PERSPECTIVE;              // perspectiva vs ortográfica
+
+    // Loop Principal que mantiene al juego activo hasta que se cierre la ventana
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+            DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 10.0f, 10.0f }, GREEN);
+            DrawCube((Vector3){ 0.0f, 0.5f, 0.0f }, 1.0f, 1.0f, 1.0f, YELLOW);
+        EndMode3D();
+
+        EndDrawing();
+    }
+
+    CloseWindow(); // Cierra la ventana y libera recursos
+
     return 0;
 }
